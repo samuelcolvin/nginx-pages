84b22d033ab027f3886150e07c0392009438ce95
Samuel Colvin <s@muelcolvin.com>
2016-04-27 20:53:51 +0100

    improving vanilla

--- vanilla.sh
+++ vanilla.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 {
+    echo "This script requires superuser access to install apt packages."
+    echo "You will be prompted for your password by sudo."
+
+    # clear any previous sudo permission
+    sudo -k
+
+    # run inside sudo
+    sudo sh <<SCRIPT
     set -e
-    apt-get update
-    apt-get -y install software-properties-common git python3.5 libssl-dev libpcre3-dev
+    echo "updating apt-get..."
+    apt-get update 1>/dev/null
+    echo "installing software-properties-common git python3.5 libssl-dev libpcre3-dev..."
+    apt-get -y install software-properties-common git python3.5 libssl-dev libpcre3-dev 1>/dev/null
     add-apt-repository -y ppa:nginx/development
-    apt-get update
+    apt-get update 1>/dev/null
+    echo "installing the latest nginx..."
     apt-get install -y nginx
     rm -rf /var/lib/apt/lists/*
     chown -R www-data:www-data /var/lib/nginx
